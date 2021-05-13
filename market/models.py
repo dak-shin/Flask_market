0798c01f2064739fc54319b837bbcdd303a05fb2
dak-shin <dakshinsa2107@gamil.com>
2021-05-13 19:59:54 +0530

    User model created and linked with Item model

--- market/models.py
+++ market/models.py
@@ -1,7 +1,19 @@
+from enum import unique
+from operator import length_hint
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 from .app import app
 
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), unique=True, nullable=False)
+    email_address = db.Column(db.String(length=50), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
 
 
 class Item(db.Model):
@@ -10,6 +22,7 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(12), nullable=False, unique=True)
     description = db.Column(db.String(1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'Item {self.name}'
