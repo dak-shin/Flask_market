3936b799289b1b47d51b63e36ae028e63e81c6ce
dak-shin <dakshinsa2107@gamil.com>
2021-05-13 20:44:42 +0530

    Register form created

--- market/app.py
+++ market/app.py
@@ -3,3 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'e49f7898dcafbe0db7e81abf'
+
+
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from .app import app
 from .models import Item
-
+from .forms import RegisterForm
 
 @app.route('/')
 @app.route('/home')
@@ -13,3 +13,8 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
